e91fe227a2e787d9183287dce6c056b7b52c9c16
jtorres <jtorres@madiva.com>
2024-07-30 14:50:52 +0200

    H3 working but it's very slow

--- client/src/components/MapH3.vue
+++ client/src/components/MapH3.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    <div v-html="mapHtml"></div>
-  </div>
+  
+  
+      <div ref="plotlyChart" style="height: 800px; width: 100%"></div>
+  
+  
 </template>
 
 <script setup>
-import { ref, onMounted, defineProps } from 'vue';
+import { ref, onMounted, defineProps, nextTick } from 'vue';
 import axios from 'axios';
-import { API_HOST, API_PORT } from '../../config';
-const apiUrl = `${API_HOST}:${API_PORT}`;
+import Plotly from 'plotly.js-dist';
 
 ///////////////////////////////////////////////////
 // Props
@@ -21,13 +22,16 @@ const props = defineProps({
 // Data
 ///////////////////////////////////////////////////
 // Propiedades reactivas
-const mapHtml = ref('');
+const plotData = ref({ data: [], layout: {} });
+const plotlyChart = ref(null);
 
 ///////////////////////////////////////////////////
 // Mount
 ///////////////////////////////////////////////////
 onMounted(async () => {
   await loadMap();
+  await nextTick(); // Esperar a que el DOM se actualice completamente
+  renderPlotlyChart(); // Renderizar el gráfico después de cargar los datos y el DOM
 });
 
 ///////////////////////////////////////////////////
@@ -35,10 +39,28 @@ onMounted(async () => {
 ///////////////////////////////////////////////////
 const loadMap = async () => {
   try {
-    const response = await axios.get(`http://localhost:8000/maps?table=${props.table}&level=5`);
-    mapHtml.value = response.data;
+    var response = await axios.get(`http://localhost:8000/maps?table=${props.table}&level=5`);
+    console.log('Map data loaded:', response.data);
+    var mapData = JSON.parse(response.data);
+    console.log('Map data parsed:', mapData);
+    plotData.value =  mapData || { data: [], layout: {} }; // Asegurarse de que los datos tengan una estructura predeterminada
+    console.log('Map loaded:', plotData.value);
   } catch (error) {
     console.error('Error loading map:', error);
+  }
+};
+
+const renderPlotlyChart = () => {
+  
+  if (plotlyChart.value && plotData.value.data && plotData.value.data.length) {
+    console.log('Rendering plotly chart:', plotData.value);
+    Plotly.newPlot(plotlyChart.value, plotData.value.data, plotData.value.layout).then(() => {
+      console.log('Plotly chart rendered successfully');
+    }).catch((error) => {
+      console.error('Error rendering plotly chart:', error);
+    });
+  } else {
+    console.log('Plotly chart container or data not ready:', plotlyChart.value, plotData.value);
   }
 };
 </script>
--- server/routes/maps_controller.py
+++ server/routes/maps_controller.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
-import geopandas as gpd
+from fastapi.responses import JSONResponse
 import h3
 from shapely.geometry import Polygon
 from shapely import wkt
@@ -9,6 +8,8 @@ from geojson import Feature, FeatureCollection
 import plotly.express as px
 from services import databaseService
 from fastapi import Request
+from plotly import graph_objs as go
+import matplotlib.colors as mcolors
 
 router = APIRouter(prefix="/maps")
 
@@ -21,7 +22,7 @@ def add_geometry(row):
 def geo_to_h3(row, H3_res):
     return h3.geo_to_h3(lat=row.LATDD83, lng=row.LONGDD83, resolution=H3_res)
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/html", response_class=HTMLResponse)
 async def create_map(table: str, level: int = 5):
     df = databaseService.runQuery(f"""
     SELECT h3_cell, h3_cell_to_boundary_wkt(h3_cell) geom, count(*) as count,
@@ -92,6 +93,105 @@ async def create_map(table: str, level: int = 5):
         print("Error: " + str(e))
         return "Error creating map"
 
+@router.get("/plotly", response_class=JSONResponse)
+async def create_map(table: str, level: int = 5):
+    df = databaseService.runQuery(f"""
+    SELECT h3_cell, h3_cell_to_boundary_wkt(h3_cell) geom, count(*) as count,
+     avg(anyoConst) as aggField, FROM
+    (SELECT *,h3_latlng_to_cell(latitud, longitud, {level}) as h3_cell FROM {table}) as subq1
+    GROUP BY h3_cell
+    ORDER BY count DESC
+    """)
+
+    features = []
+    for index, row in df.iterrows():
+        geom = wkt.loads(row['geom'])  # Convierte WKT a un objeto de geometría usando wkt.loads
+        feature = Feature(geometry=geom, properties={"aggField": row['aggField'], "h3_cell": row['h3_cell']})
+        features.append(feature)
+    geojson_obj = FeatureCollection(features)
+
+    fig = go.Figure(go.Choroplethmapbox(
+        geojson=geojson_obj,
+        locations=df['h3_cell'],
+        z=df['aggField'],
+        colorscale="Viridis",
+        zmin=0,
+        zmax=df['aggField'].max(),
+        marker_opacity=0.7,
+        marker_line_width=0
+    ))
+
+    fig.update_layout(mapbox_style="carto-positron",
+                      mapbox_zoom=5,
+                      mapbox_center={"lat": 40.624032273164794, "lon": -3.993888283105448},
+                      margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    # Enviar datos y configuración del gráfico
+    # Print fig to json
+    #print(fig.to_json())
+    return JSONResponse(content=fig.to_json())
+
+# Define la función map_value_to_color para asignar colores según el valor de 'aggField'
+def map_value_to_color(value, vmin, vmax, colorscale):
+    # Normaliza el valor entre 0 y 1
+    norm_value = (value - vmin) / (vmax - vmin)
+    # Calcula el índice en la escala de colores
+    color_idx = int(norm_value * (len(colorscale) - 1))
+    return colorscale[color_idx]
+
+@router.get("/", response_class=JSONResponse)
+async def create_map(table: str, level: int = 5):
+    df = databaseService.runQuery(f"""
+    SELECT h3_cell, h3_cell_to_boundary_wkt(h3_cell) geom, count(*) as count,
+     avg(anyoConst) as aggField FROM
+    (SELECT *,h3_latlng_to_cell(latitud, longitud, {level}) as h3_cell FROM {table}) as subq1
+    GROUP BY h3_cell
+    ORDER BY count DESC
+    """)
+
+    vmin = df['aggField'].min()
+    vmax = df['aggField'].max()
+    colorscale = ["#440154", "#482878", "#3e4989", "#31688e", "#26828e", "#1f9e89", "#35b779", "#6ece58", "#b5de2b", "#fde725"]
+
+    features = []
+    layers = []
+    for index, row in df.iterrows():
+        geom = wkt.loads(row['geom'])  # Convierte WKT a un objeto de geometría usando wkt.loads
+        color = map_value_to_color(row['aggField'], vmin, vmax, colorscale)
+        feature = Feature(geometry=geom, properties={"aggField": row['aggField'], "h3_cell": row['h3_cell']})
+        features.append(feature)
+        layers.append({
+            "sourcetype": "geojson",
+            "source": {
+                "type": "FeatureCollection",
+                "features": [feature]
+            },
+            "type": "fill",
+            "color": color,
+            "opacity": 0.7
+        })
+    geojson_obj = FeatureCollection(features)
+
+    fig = go.Figure(go.Choroplethmapbox(
+        geojson=geojson_obj,
+        locations=df['h3_cell'],
+        z=df['aggField'],
+        colorscale="Viridis",
+        zmin=0,
+        zmax=df['aggField'].max(),
+        marker_opacity=0.7,
+        marker_line_width=0
+    ))
+
+    fig.update_layout(
+        mapbox_style="carto-positron",
+        mapbox_zoom=5,
+        mapbox_center={"lat": 40.624032273164794, "lon": -3.993888283105448},
+        mapbox={"layers": layers},
+        margin={"r": 0, "t": 0, "l": 0, "b": 0}
+    )
+
+    return JSONResponse(content=fig.to_json())
 
 @router.post("/update", response_class=HTMLResponse)
 async def update_map(request: Request):
@@ -154,3 +254,4 @@ async def update_map(request: Request):
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     return fig.to_json()
+
